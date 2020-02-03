48ec33b5f34a34724d5dfacfc793f875ea2a4f96
Martin Schoen <schoenm@cip.ifi.lmu.de>
2020-02-03 21:00:48 +0100

    Use editor with basic formatting instead of plain textareas

--- src/js/components/weeekday.js
+++ src/js/components/weeekday.js
@@ -1,31 +1,48 @@
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
 import Util from '../util';
+import Constants from '../constants';
 import LocalStorageAdapter from './storage';
 
 let Storage = new LocalStorageAdapter();
+let editorConfig = {
+    modules: { toolbar: false },
+    placeholder: Constants.PLACEHOLDER_LABEL,
+    theme: 'bubble'
+};
 
 function Weeekday (title) {
 
+    // instance variables
     this.title = title;
     this.sanitizedTitle = Util.sanitize(title);
     this.node = null;
     this.editor = null;
 
+    // build UI elements
     let section = document.createElement('section');
     let headline = document.createElement('h2');
-    let editorRoot = document.createElement('textarea');
-    let updateFunction = () => {
-        Storage.setField(title, editorRoot.value);
-    }
+    let editor = document.createElement('div');
+    let editorRoot = document.createElement('div');
     section.className = 'weeek-day weeek-day--' + Util.sanitize(title);
     headline.textContent = title;
-    editorRoot.className = 'weeek-day__editor';
-    editorRoot.value = Storage.getField(title) || '';
-    editorRoot.addEventListener('keyup', updateFunction);
-    editorRoot.addEventListener('change', updateFunction);
+    editor.className = 'weeek-day__editor';
+    editorRoot.className = 'weeek-day__editor-root';
 
+    // assemble UI
     section.appendChild(headline);
-    section.appendChild(editorRoot);
+    editor.appendChild(editorRoot);
+    section.appendChild(editor);
     this.node = section;
+
+    // init editor
+    this.editor = new Quill(editorRoot, editorConfig);
+    let storedContent = Storage.getField(title) || [{insert:'\n'}];
+    this.editor.setContents(storedContent);
+    this.editor.on('text-change', () => {
+        let content = this.editor.getContents();
+        Storage.setField(title, content);
+    })
 }
 
 export default Weeekday;
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,6 @@
 export default {
     DAYS: [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 
         'Friday', 'Saturday', 'Sunday' ],
-    LOCALSTORAGE_KEY: 'weeek_content'
+    LOCALSTORAGE_KEY: 'weeek_content',
+    PLACEHOLDER_LABEL: 'You\'re free today!'
 }
