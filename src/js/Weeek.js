57a6725ab3dc5330a0ff7541219a0956cd817f33
Martin Schön <martin.schoen@spiegel.de>
2021-06-07 16:41:39 +0200

    Refactored Weeek object to ES class

--- src/js/Weeek.js
+++ src/js/Weeek.js
@@ -1,14 +1,14 @@
 import Weeekday from './components/Weeekday';
 
-function Weeek (parentElement) {
-    this.root = parentElement;
-    this.days = {};
-
-    this.addDay = (title => {
+export default class Weeek {
+    constructor(parentElement) {
+        this.root = parentElement;
+        this.days = {};
+    }
+    
+    addDay(title) {
         let dayInstance = new Weeekday(title);
         this.days[title] = dayInstance;
         this.root.appendChild(dayInstance.node);
-    }).bind(this);
-}
-
-export default Weeek;
+    }
+};
