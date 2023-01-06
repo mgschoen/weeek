a61deff7b8c61a798563ef43a3bc239f2d9d2efd
Martin Schön <martin.schoen@spiegel.de>
2023-01-06 16:47:02 +0100

    Replaced sass with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-to-109': 'linear-gradient(109.6deg, var(--tw-gradient-stops))'
+      }
+    },
+  },
+  plugins: [],
+}
