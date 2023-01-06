5ca8274fc5941e5279303625e339616a2966620e
Martin Schön <martin.schoen@spiegel.de>
2023-01-06 16:03:49 +0100

    Throw almost everything in the bin

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
 import { version } from '../../package.json';
-import Weeek from './_Weeek';
-
-const DAYS = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-];
-
-let rootElement = document.querySelector('.weeek');
-let weeek = new Weeek(rootElement);
-DAYS.forEach((dayTitle) => weeek.addDay(dayTitle));
-window.weeek = weeek;
 
 document.getElementById('version').textContent = `v${version}`;
